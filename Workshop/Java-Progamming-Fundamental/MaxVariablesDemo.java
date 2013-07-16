8cfa1dddea7acb8acee913c90a1e67822b859531
Sampe Tua Ompung Sunggu <sampe.tua212@gmail.com>
2013-07-16 18:42:56 +0700

    Create MaxVariablesDemo.java

---
+++ Workshop/Java-Progamming-Fundamental/MaxVariablesDemo.java
@@ -0,0 +1,21 @@
+public class MaxVariablesDemo {
+  public ststic void main(String[]args) {
+    //integers
+    byte largestByte     = Byte.MAX_VALUE;
+    short largestShort   = Short.MAX_VALUE;
+    int largestInteger   = Integer.MAX_VALUE;
+    long largestLong     = Long.MAX_VALUE;  
+    //real numbers
+    float largestFloat   = Float.MAX_VALUE; 
+    double largestDouble = Double.MAX_VALUE;
+    //other primitive types
+    char aChar           = 'S';
+    boolean aBoolean     = true;
+    //display then all
+    System.out.println("The Largest byte value is   = "+largestByte);
+    System.out.println("The Largest short value is   = "+largestShort);
+    System.out.println("The Largest  value is   = "+largest);
+    System.out.println("The Largest  value is   = "+largest);
+    System.out.println("The Largest  value is   = "+largest);
+    System.out.println("The Largest  value is   = "+largest);
+    System.out.println("The Largest  value is   = "+largest);
