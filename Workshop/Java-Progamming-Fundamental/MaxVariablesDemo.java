ae1d90d2e80f3730a597bbca827fbacaaabf9a84
Sampe Tua Ompung Sunggu <sampe.tua212@gmail.com>
2013-07-16 18:54:29 +0700

    Update MaxVariablesDemo.java

--- Workshop/Java-Progamming-Fundamental/MaxVariablesDemo.java
+++ Workshop/Java-Progamming-Fundamental/MaxVariablesDemo.java
@@ -14,8 +14,15 @@ public class MaxVariablesDemo {
     //display then all
     System.out.println("The Largest byte value is   = "+largestByte);
     System.out.println("The Largest short value is   = "+largestShort);
-    System.out.println("The Largest  value is   = "+largest);
-    System.out.println("The Largest  value is   = "+largest);
-    System.out.println("The Largest  value is   = "+largest);
-    System.out.println("The Largest  value is   = "+largest);
-    System.out.println("The Largest  value is   = "+largest);
+    System.out.println("The Largest integer value is   = "+largestInteger);
+    System.out.println("The Largest long value is   = "+largestLong);
+    System.out.println("The Largest float value is   = "+largestFloat);
+    System.out.println("The Largest double value is   = "+largestDouble);
+    if (Character.isUpperCase(aChar)) {
+      System.out.println("The Character "+ aChar +" is upper case.");
+    } else {
+      System.out.println("The Character "+ aChar +" is lower case.");
+    }
+    System.out.println("The Value of aBoolean is "+ aBoolean);
+  }
+}
