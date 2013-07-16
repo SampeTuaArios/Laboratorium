04b977115a13aa7bc49aa1841c99bdcc9d200edd
Sampe Tua Ompung Sunggu <sampe.tua212@gmail.com>
2013-07-16 18:30:31 +0700

    Create HelloWorld.java
    Save the file with name is HelloWorld.java

---
+++ Workshop/Java-Progamming-Fundamental/HelloWorld.java
@@ -0,0 +1,8 @@
+/*HelloWorld.java*/
+improt java.lang.System;
+
+class HelloWord {
+  public static void main(Strig[]args) {
+    System.out.println("Hello World!"); //menulis kata Hello World!
+  }
+}
